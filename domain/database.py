141e1b07cec17b686f0fee561b4022678ce04acc
uiandwe <uiandwe@gmail.com>
2020-03-17 16:46:58 +0900

    DB 리팩토링

--- domain/database.py
+++ domain/database.py
@@ -4,44 +4,50 @@ from sqlalchemy import create_engine
 from domain.db_class.stock import Stock
 from domain.db_class.stock_price import StockPrice
 import os
+from sqlalchemy.orm import sessionmaker
 
 
 class DataBase:
     now_path = os.path.dirname(os.path.realpath(__file__))
 
-    def __init__(self):
-        engine = create_engine('sqlite:///'+os.path.join(DataBase.now_path, '../data/stock.db'), echo=True)
-
+    def __init__(self, echo=True):
+        engine = create_engine('sqlite:///'+os.path.join(DataBase.now_path, '../data/stock.db'), echo=echo)
         # Base.metadata.create_all(engine)
         Stock.__table__.create(bind=engine, checkfirst=True)
         StockPrice.__table__.create(bind=engine, checkfirst=True)
 
-        from sqlalchemy.orm import sessionmaker
         Session = sessionmaker(bind=engine)
-        session = Session()
+        self.session = Session()
 
-        # stock_list = Stock(name='현승재', code='알케미 테스트', exchange='123qwe')
-        # session.add(stock_list)
-        # session.commit()
+    def insert(self, insert_data):
+        try:
+            self.session.add(insert_data)
+            self.session.commit()
+        except Exception as e:
+            print(e)
 
-        result = session.query(Stock).all()
+    def select(self):
+        result = self.session.query(Stock).all()
         if len(result) > 0:
             for row in result:
                 print(row.__dict__)
 
-        # stock_price = StockPrice(Date="2020.03.10", Open=1010.10, High=123.123, Low=100.0, Close=120.10, Volumn=123123,
-        #                          stock_id=1)
-        # session.add(stock_price)
-        # session.commit()
-
-        result = session.query(StockPrice).all()
+        result = self.session.query(StockPrice).all()
         if len(result) > 0:
             for row in result:
                 print(row.__dict__)
+        pass
 
-        session.close()
+    def close(self):
+        self.session.close()
 
 
 if __name__ == '__main__':
-    db = DataBase()
+    db = DataBase(False)
+    db.select()
+    # stock_list = Stock(name='현승재', code='알케미 테스트', exchange='123qwe')
+    # stock_price = StockPrice(Date="2020.03.10", Open=1010.10, High=123.123, Low=100.0, Close=120.10, Volumn=123123, stock_id=1)
+    # db.insert(stock_price)
+    db.select()
+    db.close()
 
